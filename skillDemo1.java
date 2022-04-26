13a2b0c07201b50486b27e57ddb9b29d9ca33535
autecht <autecht@ucsd.edu>
2022-04-26 08:33:32 -0700

    Update skillDemo1.java

--- skillDemo1.java
+++ skillDemo1.java
@@ -1,5 +1,14 @@
-public class skillDemo1.java{
-    public String concatenate(String firstString, String secondString){
-        return firstString + secondString;
+public class skillDemo1{
+    public static int findMax(int[] array){
+        if(array == null || array.length == 0){
+            return -1;
+        }
+        int max = array[0];
+        for (int num: array){
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
     }
 }
